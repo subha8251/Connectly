4b2bee7464e08b23ef998b34f7d7e8b78882bc58
Subhasish Shit <subhashit4126@gmail.com>
2024-11-10 23:16:49 +0530

    final code for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -28,6 +28,16 @@ try {
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+// -- code for deployment---
+if (process.env.Node_ENV === 'production'){
+    const dirpath= path.resolve();
+    app.use(express.static("./Frontend/dist"));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(dirpath, './Frontend/dist','index.html'));
+
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
